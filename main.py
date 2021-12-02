61248337f2ae6f023036fa82f85f2ab07c47ecab
Jimmy-2 <57232408+Jimmy-2@users.noreply.github.com>
2021-12-01 23:02:04 -0500

    make it better

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import plotly.express as px
-
 import dash
 import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import plotly.graph_objects as go
 
 app = dash.Dash(__name__)
 server = app.server
