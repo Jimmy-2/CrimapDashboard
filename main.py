cff3e284c8eb42be4eddfd0dfdf1f8fec762dd33
Jimmy-2 <57232408+Jimmy-2@users.noreply.github.com>
2021-12-01 23:34:49 -0500

    make it better

--- main.py
+++ main.py
@@ -6,89 +6,110 @@ import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import plotly.graph_objects as go
 
 app = dash.Dash(__name__)
 server = app.server
 
 #---------------------------------------------------------------
 #Taken from https://www.ecdc.europa.eu/en/geographical-distribution-2019-ncov-cases
-df = pd.read_csv("COVID-19-geographic-disbtribution-worldwide-2020-03-29.csv")
+df = pd.read_csv("NYPD_Arrest_Data__Year_to_Date_.csv")
 
-dff = df.groupby('countriesAndTerritories', as_index=False)[['deaths','cases']].sum()
+dff = df.groupby('ARREST_PRECINCT', as_index=False)[['PERP_SEX','PERP_RACE']].sum()
 print (dff[:5])
+pie = px.pie(df, names='PERP_SEX', )
+pie.update_layout(paper_bgcolor="plum")
+
+pie2 = px.pie(df, names='PERP_RACE')
+pie2.update_layout(paper_bgcolor="powderblue")
+fig = go.Figure()
 #---------------------------------------------------------------
 app.layout = html.Div([
-    html.Div([
-        dash_table.DataTable(
-            id='datatable_id',
-            data=dff.to_dict('records'),
-            columns=[
-                {"name": i, "id": i, "deletable": False, "selectable": False} for i in dff.columns
-            ],
-            editable=False,
-            filter_action="native",
-            sort_action="native",
-            sort_mode="multi",
-            row_selectable="multi",
-            row_deletable=False,
-            selected_rows=[],
-            page_action="native",
-            page_current= 0,
-            page_size= 6,
-            # page_action='none',
-            # style_cell={
-            # 'whiteSpace': 'normal'
-            # },
-            # fixed_rows={ 'headers': True, 'data': 0 },
-            # virtualization=False,
-            style_cell_conditional=[
-                {'if': {'column_id': 'countriesAndTerritories'},
-                 'width': '40%', 'textAlign': 'left'},
-                {'if': {'column_id': 'deaths'},
-                 'width': '30%', 'textAlign': 'left'},
-                {'if': {'column_id': 'cases'},
-                 'width': '30%', 'textAlign': 'left'},
-            ],
-        ),
-    ],className='row'),
+    html.Div(
+        children=[
 
-    html.Div([
-        html.Div([
-            dcc.Dropdown(id='linedropdown',
-                options=[
-                         {'label': 'Deaths', 'value': 'deaths'},
-                         {'label': 'Cases', 'value': 'cases'}
-                ],
-                value='deaths',
-                multi=False,
-                clearable=False
+            html.H1(
+                children="NYPD Arrest Data (Year to Date)", className="header-title"
             ),
-        ],className='six columns'),
 
-        html.Div([
-        dcc.Dropdown(id='piedropdown',
-            options=[
-                     {'label': 'Deaths', 'value': 'deaths'},
-                     {'label': 'Cases', 'value': 'cases'}
-            ],
-            value='cases',
-            multi=False,
-            clearable=False
-        ),
-        ],className='six columns'),
+        ],
+        className="header",
+    ),
+    dcc.Tabs([
+        dcc.Tab(label='Graphs and Charts', children=[
+            html.Div([
+                html.Div([
+                    dcc.Graph(figure=pie),
+                ],
+                    style={'width': '49%', 'display': 'inline-block'}),
 
-    ],className='row'),
+                html.Div([
 
-    html.Div([
-        html.Div([
-            dcc.Graph(id='linechart'),
-        ],className='six columns'),
+                    dcc.Graph(figure=pie2),
+                ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'})
 
-        html.Div([
-            dcc.Graph(id='piechart'),
-        ],className='six columns'),
+            ]),
 
-    ],className='row'),
+            html.Div([
+
+                html.Div([
+                    html.Div([
+                        dcc.Dropdown(id='linedropdown',
+                                     options=[
+                                         {'label': 'Sex', 'value': 'PERP_SEX'},
+                                         {'label': 'Race', 'value': 'PERP_RACE'}
+                                     ],
+                                     value='deaths',
+                                     multi=False,
+                                     clearable=False
+                                     ),
+                    ], className='six columns'),
+
+                    html.Div([
+                        dcc.Dropdown(id='piedropdown',
+                                     options=[
+                                         {'label': 'Sex', 'value': 'PERP_SEX'},
+                                         {'label': 'Race', 'value': 'PERP_RACE'}
+                                     ],
+                                     value='cases',
+                                     multi=False,
+                                     clearable=False
+                                     ),
+                    ], className='six columns'),
+
+                ], className='row'),
+
+                html.Div([
+                    html.Div([
+                        dcc.Graph(id='linechart'),
+                    ], className='six columns'),
+
+                    html.Div([
+                        dcc.Graph(id='piechart'),
+                    ], className='six columns'),
+
+                ], className='row'),
+            ], style={
+                'padding': '10px 5px'
+            }),
+
+            dcc.Graph(
+                figure=fig,
+                id='crossfilter-indicator-scatter',
+                hoverData={'points': [{'customdata': 'shot'}]}
+
+            )
+
+        ]),
+        dcc.Tab(
+
+            label='Map', children=[
+
+            ]),
+
+    ]),
+
+
 
 
 ])
@@ -101,9 +122,24 @@ app.layout = html.Div([
      Input('piedropdown', 'value'),
      Input('linedropdown', 'value')]
 )
+def create_time_series(dff, axis_type, title):
+    fig = px.bar(dff, x='ARREST_PRECINCT', y='PERP_SEX', color="PERP_SEX")
+
+    fig.update_xaxes(showgrid=False)
+
+    fig.update_yaxes(type='linear' if axis_type == 'Linear' else 'log')
+
+    fig.add_annotation(x=0, y=0.85, xanchor='left', yanchor='bottom',
+                       xref='paper', yref='paper', showarrow=False, align='left',
+                       text=title)
+
+    fig.update_layout(height=225, margin={'l': 20, 'b': 30, 'r': 10, 't': 10}, paper_bgcolor="rosybrown")
+
+    return fig
+
 def update_data(chosen_rows,piedropval,linedropval):
     if len(chosen_rows)==0:
-        df_filterd = dff[dff['countriesAndTerritories'].isin(['China','Iran','Spain','Italy'])]
+        df_filterd = dff[dff['AGE_GROUP'].isin(['China','Iran','Spain','Italy'])]
     else:
         print(chosen_rows)
         df_filterd = dff[dff.index.isin(chosen_rows)]
@@ -125,7 +161,7 @@ def update_data(chosen_rows,piedropval,linedropval):
 
     line_chart = px.line(
             data_frame=df_line,
-            x='dateRep',
+            x='ARREST_DATE',
             y=linedropval,
             color='countriesAndTerritories',
             labels={'countriesAndTerritories':'Countries', 'dateRep':'date'},
