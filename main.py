c0ae0649b576c43ca8c15d4a07f8f5317723e907
Jimmy-2 <57232408+Jimmy-2@users.noreply.github.com>
2021-12-02 00:35:16 -0500

    make it better

--- main.py
+++ main.py
@@ -23,6 +23,8 @@ pie.update_layout(paper_bgcolor="plum")
 pie2 = px.pie(df, names='PERP_RACE')
 pie2.update_layout(paper_bgcolor="powderblue")
 fig = go.Figure()
+fig2 = px.histogram(df, x='PERP_RACE', nbins=20)
+fig3 = px.histogram(df, x='PERP_SEX', nbins=20)
 #---------------------------------------------------------------
 app.layout = dash_table.DataTable(
     id='table',
@@ -54,21 +56,22 @@ app.layout = html.Div([
                 ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'})
 
             ]),
-
             html.Div([
+                html.Div([
+                    dcc.Graph(figure=fig3)
+
+                ],
+                    style={'width': '49%', 'display': 'inline-block'}),
+
+                html.Div([
+
+                    dcc.Graph(figure=fig2)
+                ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'})
+
+            ]),
 
 
 
-            ], style={
-                'padding': '10px 5px'
-            }),
-
-            dcc.Graph(
-                figure=fig,
-                id='crossfilter-indicator-scatter',
-                hoverData={'points': [{'customdata': 'shot'}]}
-
-            )
 
         ]),
         dcc.Tab(
@@ -83,6 +86,59 @@ app.layout = html.Div([
 
 
 ])
+
+
+@app.callback(
+    Output('graph1', 'figure'),
+    [Input('dropdown1', 'value')]
+)
+# graph plot and styling
+def update_graph(value):
+    if value == 'cus':
+        xx = ['North indian', 'chinese', 'continental', 'cafe', 'fast food', 'south indian', 'italian', 'desserts',
+              'biryani', 'beverages']
+        y1 = x1
+        col = 'lightcoral'
+    if value == 'dish':
+        xx = ['pasta', 'burgers', 'cocktails', 'pizza', 'biryani', 'coffee', 'mocktails', 'sandwiches', 'paratha',
+              'noodles']
+        y1 = s1
+        col = 'skyblue'
+    return {'data': [go.Bar(
+        x=xx,
+        y=y1,
+        marker_color=col),
+
+    ],
+        'layout': go.Layout(
+            title='Top 10 cuisines',
+            plot_bgcolor='#26332a',
+            paper_bgcolor='#26332a',
+            xaxis_tickangle=-17,
+
+            xaxis=dict(
+                # type='line',
+                title='Most liked dish',
+                showgrid=True,
+                showline=True,
+                color='white',
+                linewidth=1,
+
+            ),
+            yaxis=dict(
+                title='Count',
+                showgrid=True,
+                showline=True,
+                gridcolor='#bdbdbd',
+                color='white',
+                linewidth=1
+            ),
+            margin={'l': 60, 'b': 40, 't': 30, 'r': 60},
+            # legend={'x': 0.5, 'y': 1},
+            hovermode='closest',
+
+        )
+    }
 
 
 if __name__ == '__main__':
