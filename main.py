0d9ecb122b0f31369594db9452803a3e6f57d31b
Jimmy-2 <57232408+Jimmy-2@users.noreply.github.com>
2021-12-10 00:55:13 -0500

    make it better

--- main.py
+++ main.py
@@ -8,12 +8,11 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.graph_objects as go
 
+
 app = dash.Dash(__name__)
 server = app.server
 
-#---------------------------------------------------------------
-#Taken from https://www.ecdc.europa.eu/en/geographical-distribution-2019-ncov-cases
-df = pd.read_csv("NYPD_Arrest_Data__Year_to_Date_.csv")
+df = pd.read_csv("https://data.cityofnewyork.us/api/views/uip8-fykc/rows.csv?accessType=DOWNLOAD&bom=true&format=true")
 
 dff = df.groupby('ARREST_PRECINCT', as_index=False)[['PERP_SEX','PERP_RACE']].sum()
 print (dff[:5])
@@ -25,7 +24,12 @@ pie2.update_layout(paper_bgcolor="powderblue")
 fig = go.Figure()
 fig2 = px.histogram(df, x='PERP_RACE', nbins=20, color="PERP_RACE")
 fig3 = px.histogram(df, x='PERP_SEX', nbins=20, color="PERP_SEX")
-#---------------------------------------------------------------
+
+px.set_mapbox_access_token("pk.eyJ1IjoiZWNjaGlmdWNrZXIiLCJhIjoiY2t3endqbGhxMGtpeTJ2bXJqOGg2b2RkZiJ9.PzATagpmMHlKAM4KB6AO9A")
+df['Description'] = ' '+ df['PERP_SEX'] + ', ' + df['PERP_RACE'] + ', ' + df['AGE_GROUP'] + ', ' + df['PD_DESC'].astype(str)
+fig5 = px.scatter_mapbox(df, lat="Latitude", lon="Longitude", color="ARREST_PRECINCT", text = "Description",)
+
+
 app.layout = dash_table.DataTable(
     id='table',
     columns=[{"name": i, "id": i} for i in df.columns],
@@ -37,14 +41,51 @@ app.layout = html.Div([
             html.H1(
                 children="Crimap Dashboard", className="header-title"
             ),
-            html.H2(
-                children="NYPD Arrest Data (Year to Date)", className="header-subtitle"
-            ),
+            html.H6(children="NYPD Arrest Data (Year to Date)"),
+            html.Img(src='/assets/arrestImage.jpg')
+
 
         ],
         className="header",
     ),
     dcc.Tabs([
+        dcc.Tab(
+
+            label='Overview', children=[
+                html.Div([
+
+                    html.Div([
+                        html.H6(children="Hypothesis"),
+                        html.P('While creating visual representations of the arrests data, I have come to a conclusion that the people that get arrested are more likely to be male than female.'),
+
+                    ],
+                    style={'width': '49%', 'display': 'inline-block'}),
+                    html.Div([
+                        html.H6(children="Dataset Analysis"),
+                        html.P('While I was able to easily create a scatterplot map with the latitude and longtitude columns, I was unable to easily create a non-histogram chart/graph due to the lack of columns with numerical data. The only columns with numerical data are area descriptions/locations.'),
+
+                    ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'})
+                ]),
+                html.Div([
+                    html.Div([
+                        html.H6(children="Techniques"),
+                        html.P('The entire project is coded in Python DASH and deployed through heroku. The graphs, charts and map are created with the help of plotly.'),
+                    ],
+                        style={'width': '49%', 'display': 'inline-block'}),
+
+                    html.Div([
+
+                        html.H6(children="Citations"),
+                        html.P('Dataset: https://data.cityofnewyork.us/Public-Safety/NYPD-Arrest-Data-Year-to-Date-/uip8-fykc'),
+                        html.P('Dash HTML Guide: https://dash.plotly.com/dash-html-components'),
+                        html.P('Plotly Histogram Guide: https://plotly.com/python/histograms/'),
+                        html.P('Map Guide: https://towardsdatascience.com/scatter-plots-on-maps-using-plotly-79f16aee17d0'),
+                        html.P('Heroku Guide: https://www.youtube.com/watch?v=b-M2KQ6_bM4')
+                    ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'})
+
+                ]),
+
+            ]),
         dcc.Tab(label='Graphs and Charts', children=[
 
             html.Div([
@@ -89,7 +130,10 @@ app.layout = html.Div([
         dcc.Tab(
 
             label='Map', children=[
+                html.Div([
 
+                    dcc.Graph(figure=fig5),
+                ],)
             ]),
 
     ]),
